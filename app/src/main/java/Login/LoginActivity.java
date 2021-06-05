78ef627de93f0b433a92c62917cbf28edcdd5fb0
diberardinocalisti <diberardinocalisti@gmail.com>
2021-06-06 00:36:02 +0200

    git commit -a -m "Login Facebook completato"

--- app/src/main/java/Login/LoginActivity.java
+++ app/src/main/java/Login/LoginActivity.java
@@ -1,24 +1,63 @@
 package Login;
 
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.briscolav10.R;
-
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+    private TextView i;
+    private LoginButton l;
+    private CallbackManager callbackManager;
+    private ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
-        System.out.println("ready");
 
-        //Game.startGame(this);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        i = (TextView)findViewById(R.id.info);
+        l = (LoginButton)findViewById(R.id.login_button);
+        image = (ImageView) findViewById(R.id.img);
+
+        l.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                i.setText("User ID: " + loginResult.getAccessToken().getUserId() + "\n" + "Auth Token: " + loginResult.getAccessToken().getToken());
+                String imgUrl = "https://graph.facebook.com/" + loginResult.getAccessToken().getUserId() + "/picture?return_ssl_resources=1";
+                Picasso.get().load(imgUrl).into(image);
+            }
+
+            @Override
+            public void onCancel() {
+                i.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                i.setText("Login attempt failed.");
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode , resultCode , data);
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
