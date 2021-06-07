717e70c65d4443423d1ac9fbbdeb8f3360a520cb
simonediberardino <simonediberardino7@gmail.cocm>
2021-06-08 00:43:06 +0200

    Aggiornamento layout partita (icone posizionate nella giusta posizione in ogni dispositivo)

--- app/src/main/java/Home/MainMenu.java
+++ app/src/main/java/Home/MainMenu.java
@@ -19,33 +19,33 @@ public class MainMenu extends AppCompatActivity {
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void startGame(Button b, MainActivity main)
     {
-        String tmp = b.getText().toString();
+        String tmp = b.getText().toString().toLowerCase();
 
         switch(tmp){
-            case "1 VS CPU":
+            case "singleplayer":
                 Intent intent = new Intent(main, ActivityGame.class);
                 intent.putExtra("multiplayer",false);
                 main.startActivity(intent);
                 break;
 
-            case "MULTIPLAYER":
+            case "multiplayer":
                 Intent i = new Intent(main, MultiplayerActivity.class);
                 main.startActivity(i);
                 break;
 
-            case "Come si gioca?":
-                String title = "Come si gioca?";
+            case "come si gioca?":
+                String title = "Introduzione a Briscola";
                 String msg = "Per giocare a Briscola si utilizza un mazzo di 40 carte diviso in 4 semi, ciascuno di 10 carte.\nNel mazzo vi sono un totale di 120 punti, pertanto vincerà il round chi ne realizza almeno 61. \nSe i punti sono 60 il round è considerato pari.\n\nDi seguito i punteggi:\nAsso: 11 punti;\nTre: 10 punti;\nRe: 4 punti;\nCavallo: 3 punti;\nFante: 2 punti;\n\nBUON DIVERTIMENTO!";
 
                 Utility.createDialog(main,title, msg);
                 break;
 
-            case "Esci dal gioco":
+            case "esci dal gioco":
                 android.os.Process.killProcess(android.os.Process.myPid());
                 System.exit(1);
                 break;
 
-            case "Il mio profilo":
+            case "il mio profilo":
                 Intent in = new Intent(main, LoginActivity.class);
                 main.startActivity(in);
                 //Toast.makeText(main,"prova",Toast.LENGTH_SHORT).show();
