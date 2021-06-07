eecdc8525d5003e71dc892713c57883250c9bdc4
diberardinocalisti <diberardinocalisti@gmail.com>
2021-06-07 16:51:12 +0200

    Login con google

--- app/src/main/java/Login/LoginActivity.java
+++ app/src/main/java/Login/LoginActivity.java
@@ -2,10 +2,12 @@ package Login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.briscolav10.R;
@@ -15,26 +17,49 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.login.widget.ProfilePictureView;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import Home.MainActivity;
 import gameEngine.Utility;
 
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private CallbackManager callbackManager;
+
+    SignInButton sButton;
+    private GoogleApiClient gApi;
+    private static final int SIGN_IN = 1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         if(!loginClass.isFacebookLoggedIn())
         {
             TextView i;
             LoginButton l;
             setContentView(R.layout.login_page);
+
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+            gApi = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,gso).build();
+
+            sButton = (SignInButton) findViewById(R.id.googleLogin);
+
+            sButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = Auth.GoogleSignInApi.getSignInIntent(gApi);
+                    startActivityForResult(i, SIGN_IN);
+                }
+            });
 
             this.registerButtons();
 
@@ -61,9 +86,6 @@ public class LoginActivity extends AppCompatActivity {
                 }
             });
         }else{
-            if (!loginClass.isFacebookLoggedIn())
-                Toast.makeText(getApplicationContext(),"NOOOO",Toast.LENGTH_SHORT).show();
-
             logIn();
         }
 
@@ -74,7 +96,25 @@ public class LoginActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode , resultCode , data);
 
-        callbackManager.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == SIGN_IN)
+        {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }else
+        {
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
+    private void handleSignInResult(GoogleSignInResult result){
+        if(result.isSuccess()){
+            Intent i = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(i);
+            Toast.makeText(getBaseContext(),"Ti sei loggato con successo!",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(getApplicationContext(),"Sign in cancel",Toast.LENGTH_LONG).show();
+        }
     }
 
 
@@ -109,5 +149,10 @@ public class LoginActivity extends AppCompatActivity {
 
         Button back = findViewById(R.id.button2);
         back.setOnClickListener(v -> super.onBackPressed());
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull @org.jetbrains.annotations.NotNull ConnectionResult connectionResult) {
+
     }
 }
