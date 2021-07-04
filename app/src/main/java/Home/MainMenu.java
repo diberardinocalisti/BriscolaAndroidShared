9ceef1f8873db0736d6b2df2a6385d31ccef00d5
diberardinocalisti <diberardinocalisti@gmail.com>
2021-07-04 14:40:16 +0200

    Prova 1

--- app/src/main/java/Home/MainMenu.java
+++ app/src/main/java/Home/MainMenu.java
@@ -39,7 +39,7 @@ public class MainMenu {
                     Intent i = new Intent(main, MultiplayerActivity.class);
                     main.startActivity(i);
                 }else{
-                    Intent i = new Intent(main, ProvaLoginFirebase.class);
+                    Intent i = new Intent(main, LoginActivity.class);
                     main.startActivity(i);
                 }
                 break;
@@ -58,7 +58,7 @@ public class MainMenu {
 
             // Il mio profilo;
             case "button2":
-                Intent in = new Intent(main, ProvaLoginFirebase.class);
+                Intent in = new Intent(main, LoginActivity.class);
                 main.startActivity(in);
                 break;
         }
