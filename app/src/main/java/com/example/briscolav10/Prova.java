a38fa1c3b9bb8d221e1963d35ba5a2e6ca8a6f81
simonediberardino <simonediberardino7@gmail.cocm>
2021-08-04 21:57:57 +0200

    aggiornamento progetto

---
+++ app/src/main/java/com/example/briscolav10/Prova.java
@@ -0,0 +1,18 @@
+package com.example.briscolav10;
+
+public class Prova {
+
+    private String a;
+
+    public Prova(String a) {
+        this.a = a;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+}
