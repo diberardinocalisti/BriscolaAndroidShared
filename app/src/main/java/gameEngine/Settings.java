e566782c8568451b1bf76f28b5553971ffae91dd
simonediberardino <simonediberardino7@gmail.cocm>
2021-08-29 21:32:30 +0200

    restyling bottoni (PROBABILMENTE DA RIVEDRE) + restyling settings + cambio colore bottoni all'onclick + aggiunte pubblicità main activity

--- app/src/main/java/gameEngine/Settings.java
+++ app/src/main/java/gameEngine/Settings.java
@@ -3,7 +3,10 @@ package gameEngine;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,15 +30,18 @@ public class Settings {
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void createSettingsMenu(Context c)
     {
-        AlertDialog.Builder builder = new AlertDialog.Builder(c);
+        Dialog dialog = new Dialog(c);
         LayoutInflater inflater = (LayoutInflater) c.getSystemService( LAYOUT_INFLATER_SERVICE );
 
-        // VIEW;
-        View tipoCarteView = inflater.inflate(R.layout.settings, null);
+        dialog.setContentView(R.layout.settings);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
 
+        // PARENT VIEW;
+        View settings = inflater.inflate(R.layout.settings, null);
+        
         // SPINNER TIPO CARTE;
-        Spinner tipoCarte = tipoCarteView.findViewById(R.id.typeCardSpinner);
-        ArrayAdapter<String> adapterTipo = new ArrayAdapter<String>(c, android.R.layout.simple_spinner_item,c.getResources().getStringArray(R.array.tipoCarte));
+        Spinner tipoCarte = dialog.findViewById(R.id.settings_typeCardSpinner);
+        ArrayAdapter<String> adapterTipo = new ArrayAdapter<>(c, android.R.layout.simple_spinner_item, c.getResources().getStringArray(R.array.tipoCarte));
         adapterTipo.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         tipoCarte.setAdapter(adapterTipo);
         String selectedTipo = SharedPref.getTipoCarte();
@@ -46,19 +52,19 @@ public class Settings {
         tipoCarte.setSelection(indexTipo);
 
         // CHECKBOX;
-        CheckBox carteScoperte = tipoCarteView.findViewById(R.id.showCardCheckbox);
+        CheckBox carteScoperte = dialog.findViewById(R.id.settings_showCardCheckbox);
         carteScoperte.setChecked(SharedPref.getCarteScoperte());
-
-
+        
         // SPINNER SKILL CPU;
-        Spinner skillCpu = tipoCarteView.findViewById(R.id.cpuSkillSpinner);
+        Spinner skillCpu = dialog.findViewById(R.id.settings_cpuSkillSpinner);
         ArrayAdapter<String> adapterSkill = new ArrayAdapter<>(c, android.R.layout.simple_spinner_item, c.getResources().getStringArray(R.array.difficulties));
         adapterSkill.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         skillCpu.setAdapter(adapterSkill);
         skillCpu.setSelection(SharedPref.getCPUSkill());
 
-        // CONFIRM;
-        builder.setPositiveButton(c.getString(R.string.ok), (dialog, id) -> {
+        // CONFIRM BUTTON;
+        View confirmButton = dialog.findViewById(R.id.settings_accept);
+        confirmButton.setOnClickListener(v -> {
             if(carteScoperte.isChecked()){
                 SharedPref.setCarteScoperte(true);
                 if(Game.CPU != null)
@@ -76,12 +82,19 @@ public class Settings {
 
             String tipoCarteValue = tipoCarte.getSelectedItem().toString();
             Engine.aggiornaTipoCarte(tipoCarteValue);
+
+            dialog.dismiss();
         });
+        
+        View closeButton = dialog.findViewById(R.id.settings_close);
+        View cancelButton = dialog.findViewById(R.id.settings_cancel);
+        
+        View.OnClickListener closeAction = v -> dialog.dismiss();
 
-        builder.setNegativeButton(c.getString(R.string.cancel), null);
-        builder.setView(tipoCarteView);
-
-        builder.create().show();
+        closeButton.setOnClickListener(closeAction);
+        cancelButton.setOnClickListener(closeAction);
+        
+        dialog.create();
+        dialog.show();
     }
-
 }
