fc27fc89f4e266d1b99d8c781ba32fabf56fabca
Daniele <diberardinocalisti@gmail.com>
2021-09-24 22:24:34 +0200

    Implementazione vendite in-app da finire

--- app/src/main/java/Home/Shop.java
+++ app/src/main/java/Home/Shop.java
@@ -16,20 +16,67 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import Login.loginClass;
 import UI.UiColor;
+import firebase.FirebaseClass;
 import game.danielesimone.briscola.GameActivity;
 import game.danielesimone.briscola.R;
+import gameEngine.SharedPref;
 import gameEngine.Utility;
+import multiplayer.User;
 import okhttp3.internal.Util;
 
+import static Login.LoginActivity.fbUID;
 import static Login.loginClass.setImgProfile;
 import static gameEngine.Game.activity;
 
+import com.android.billingclient.api.AcknowledgePurchaseParams;
+import com.android.billingclient.api.AcknowledgePurchaseResponseListener;
+import com.android.billingclient.api.BillingClient;
+import com.android.billingclient.api.BillingClientStateListener;
+import com.android.billingclient.api.BillingFlowParams;
+import com.android.billingclient.api.BillingResult;
+import com.android.billingclient.api.ConsumeParams;
+import com.android.billingclient.api.ConsumeResponseListener;
+import com.android.billingclient.api.Purchase;
+import com.android.billingclient.api.PurchasesUpdatedListener;
+import com.android.billingclient.api.SkuDetails;
+import com.android.billingclient.api.SkuDetailsParams;
+import com.android.billingclient.api.SkuDetailsResponseListener;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import multiplayer.User;
+
 public class Shop extends GameActivity{
+    BillingClient billingClient;
+
+    List<SkuDetails> info;
+
+    Integer coinShopped = 0;
+
+    /*
+        INFO
+    *   0 --> 150 monete, 0,99
+    *   1 --> 500 monete, 1,99
+    *   2 --> remove_ads
+    * */
     @SuppressLint({"ResourceType", "UseCompatLoadingForDrawables"})
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -40,11 +87,157 @@ public class Shop extends GameActivity{
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.shop);
 
+        billingClient = BillingClient.newBuilder(this)
+                            .enablePendingPurchases()
+                            .setListener(new PurchasesUpdatedListener() {
+                                @Override
+                                public void onPurchasesUpdated(@NonNull BillingResult billingResult, @Nullable List<Purchase> list) {
+                                    if(billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && list != null)
+                                    {
+                                        for(Purchase p: list)
+                                        {
+                                            if(p.getPurchaseState() == Purchase.PurchaseState.PURCHASED && !p.isAcknowledged())
+                                            {
+                                                verifyPurchase(p);
+
+                                                FirebaseClass.getFbRefSpeicific(fbUID).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                                                        if(task.isSuccessful())
+                                                        {
+                                                            for(DataSnapshot d: task.getResult().getChildren())
+                                                            {
+                                                                if(d.hasChild("monete"))
+                                                                {
+                                                                    if(d.getKey().equals("monete"))
+                                                                    {
+                                                                        int monete = (int) d.getValue();
+                                                                        FirebaseClass.editFieldFirebase(fbUID,"monete",(monete + coinShopped) );
+                                                                    }
+                                                                }
+                                                            }
+
+
+                                                        }
+                                                    }
+                                                });
+
+                                                SharedPref.setCoin(SharedPref.getCoin() + coinShopped);
+
+
+                                                Utility.goTo(Shop.this,MainActivity.class);
+                                            }
+                                        }
+                                    }
+                                }
+                            })
+                            .build();
+
+        connectToGooglePlayBilling();
+
         Utility.enableTopBar(this);
         Utility.ridimensionamento(this, findViewById(R.id.parent));
         Utility.showAd(this);
-
         mostraProdotti();
+    }
+
+    private void connectToGooglePlayBilling()
+    {
+        billingClient.startConnection(
+                new BillingClientStateListener() {
+                    @Override
+                    public void onBillingServiceDisconnected() {
+                        connectToGooglePlayBilling();
+                    }
+
+                    @Override
+                    public void onBillingSetupFinished(@NonNull BillingResult billingResult) {
+                        if(billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK)
+                            getProductDetails();
+                    }
+                }
+        );
+    }
+
+    //
+    private void verifyPurchase(Purchase p)
+    {
+        String reqUrl = "https://us-central1-briscola-75019.cloudfunctions.net/verifyPurchase?" +
+                "purchaseToken=" + p.getPurchaseToken() +" &" +
+                "purchaseTime=" + p.getPurchaseTime()+ "&" +
+                "orderId=" + p.getOrderId();
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                reqUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject purchaseInfoFromServer = new JSONObject(response);
+                            if(purchaseInfoFromServer.getBoolean("isValid"))
+                            {
+                                ConsumeParams consumeParams = ConsumeParams.newBuilder().setPurchaseToken(p.getPurchaseToken()).build();
+                                billingClient.consumeAsync(
+                                        consumeParams,
+                                        new ConsumeResponseListener() {
+                                            @Override
+                                            public void onConsumeResponse(@NonNull BillingResult billingResult, @NonNull String s) {
+                                                if(billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK)
+                                                {
+                                                    Utility.oneLineDialog(getApplicationContext(), String.valueOf(R.string.verifiedConsume), null);
+                                                }
+                                            }
+                                        }
+                                );
+                            }
+
+                        }catch (Exception e)
+                        {
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+
+        Volley.newRequestQueue(this).add(stringRequest);
+
+    }
+
+
+    private void getProductDetails()
+    {
+        List<String> productIds = new ArrayList<>();
+        productIds.add("remove_ads");
+        productIds.add("monete_1");
+        productIds.add("monete_2");
+
+        SkuDetailsParams getProoductDetailsQuery = SkuDetailsParams
+                            .newBuilder()
+                            .setSkusList(productIds)
+                            .setType(BillingClient.SkuType.INAPP)
+                            .build();
+
+        billingClient.querySkuDetailsAsync(
+                getProoductDetailsQuery,
+                new SkuDetailsResponseListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+                    @Override
+                    public void onSkuDetailsResponse(@NonNull BillingResult billingResult, @Nullable List<SkuDetails> list) {
+                        if(billingResult.getResponseCode() == BillingClient.BillingResponseCode.OK && list != null)
+                        {
+                            System.out.println(list);
+                            info = list;
+                        }
+                    }
+                }
+        );
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -58,13 +251,14 @@ public class Shop extends GameActivity{
         mostraAdRemover(0.99);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void mostraAvatar(String avatarName, String avatarId, double avatarPrice){
         Drawable avatarDrawable = getResources().getDrawable(getResources().getIdentifier(avatarId, "drawable", getPackageName()));
 
         Runnable onClick = () -> {
-            // Elaborare il click del box degli avatar;
-            Utility.oneLineDialog(this, "E' stato selezionato un avatar", null);
-        };
+                // Elaborare il click del box degli avatar;
+                Utility.oneLineDialog(this, "E' stato selezionato un avatar", null);
+            };
 
         mostraProdotto(avatarName,
                 R.id.avatarshop_Name,
@@ -81,7 +275,13 @@ public class Shop extends GameActivity{
     private void mostraMonete(int nMonete, double price){
         Runnable onClick = () -> {
             // Elaborare il click del box delle monete;
-            Utility.oneLineDialog(this, "Sono state selezionate le monete", null);
+            // Utility.oneLineDialog(this, "Sono state selezionate le monete --> " + nMonete, null);
+            SkuDetails itemInfo = (nMonete == 150 ? info.get(0) : nMonete == 500 ? info.get(1) : null);
+
+            coinShopped = nMonete;
+
+            billingClient.launchBillingFlow(this, BillingFlowParams.newBuilder().setSkuDetails(itemInfo).build());
+
         };
 
         mostraProdotto(String.valueOf(nMonete),
@@ -99,7 +299,12 @@ public class Shop extends GameActivity{
     private void mostraAdRemover(double price){
         Runnable onClick = () -> {
             // Elaborare il click del box delle monete;
-            Utility.oneLineDialog(this, "E' stato selezionato l'ad remover", null);
+            //Utility.oneLineDialog(this, "E' stato selezionato l'ad remover", null);
+
+            SkuDetails itemInfo = info.get(2);
+
+            billingClient.launchBillingFlow(this, BillingFlowParams.newBuilder().setSkuDetails(itemInfo).build());
+
         };
 
         mostraProdotto(String.valueOf(price),
